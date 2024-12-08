b348bdb67c003341b5e43e9a8eaf59320bba00e4
root <root@DESKTOP-DTC4VL1.>
2024-12-08 20:04:25 +0100

    cambios en mi Index

--- frontend/app/principal/Index.tsx
+++ frontend/app/principal/Index.tsx
@@ -2,18 +2,18 @@ import ConnectButton from "../ConnectButton";
 
 export default function Index() {
   return (
-    <div
-      className="min-h-screen bg-cover bg-center"
-      style={{
-        backgroundImage: 'url("https://img.freepik.com/free-vector/medical-science-healthcare-background-concept_1017-30385.jpg?t=st=1733649124~exp=1733652724~hmac=ecac6d03533213444dab64312c6cc19d2b2d7192a45e5975aa52542dae18080c&w=1800")',
-      }}
-    >
-      <div className="flex items-center justify-center min-h-screen bg-black bg-opacity-50">
-        <div className="text-center text-white p-16 rounded-2xl backdrop-blur-sm border-[0.5px]">
-          <h1 className="text-4xl mb-4 p-8">Med-Records</h1>
-          <ConnectButton />
-        </div>
+    <>
+      {/* Encabezado */}
+      {/* Contenido principal */}
+      <div
+        className="min-h-screen bg-cover bg-center"
+        style={{
+          backgroundImage:
+            'url("https://img.freepik.com/free-photo/3d-plexus-background-with-connecting-lines-dots-network-communications_1048-12336.jpg?t=st=1733678005~exp=1733681605~hmac=efa76bfcefc36de7ca0513a43ebe3a0a48410d8aef2cb2b7a30b8207a1fa3c7e&w=900")',
+        }}
+      >
+        {/* Aquí puedes añadir más contenido si es necesario */}
       </div>
-    </div>
+    </>
   );
 }
