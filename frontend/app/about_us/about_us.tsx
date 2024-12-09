9d618b9d3388e25477635a4eff3a275dc2f808ee
SherlockPE <fabriciolopezreyes@gmail.com>
2024-12-09 14:00:47 +0100

    solved vercel error

--- frontend/app/about_us/about_us.tsx
+++ frontend/app/about_us/about_us.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronRight, Users, Lightbulb, Target } from 'lucide-react';
+import { ChevronRight, Lightbulb, Target } from 'lucide-react';
 
 import OliverImage from '@/assets/Oliver.jpg';
 import JohiImage from '@/assets/Johi.jpg';
